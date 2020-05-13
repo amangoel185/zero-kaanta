c3ffe534997555bb4955806078b22e4d844ddfba
Aman Goel <aman.goel185@gmail.com>
2020-05-13 23:16:19 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                {/* to-do */}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square />;
+    }
+
+    render() {
+        const status = 'next player: x';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-information">
+                    <div>{/* status */}</div>
+                    <ol>{/* to-do */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ===============================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
